f032aa689ca408e475b83d012a36216f91dc1389
oelaimar <oussama.el.aimar@gmail.com>
2026-01-14 17:49:11 +0100

    feat: add connection to database and schema of database

---
+++ app/Core/Database.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Core;
+use PDO;
+use PDOException;
+
+define('HOST', 'mysql');
+define('USERNAME', 'app_user');
+define('PASSWORD', 'password');
+define('DBNAME', 'app_db');
+
+class Database
+{
+    private PDO $pdo;
+    private static ?Database $instance = null;
+
+    private function __construct()
+    {
+        try {
+            $dsn = "mysql:host=" . HOST . ";dbname=" . DBNAME . ";charset=utf8mb4";
+            $this->pdo = new PDO($dsn, USERNAME, PASSWORD);
+            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->pdo->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+
+            die("there is problem with connection" . $e->getMessage());
+        }
+    }
+
+    public static function getInstanse(): Database
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database;
+        }
+        return self::$instance;
+    }
+
+    public function getConn(): PDO
+    {
+        return $this->pdo;
+    }
+}
